156c62b8548a47a6ed90d8ab1a43b979a3e27e6b
André Santos <contact.andre.santos@gmail.com>
2021-01-25 13:59:41 +0000

    added a bunch of scripts to execute HAROS

---
+++ scripts/fictibot.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# check if haros is installed
+if python -c 'import pkgutil; exit(not pkgutil.find_loader("haros"))'; then
+    echo 'Found HAROS in the system.'
+else
+    echo 'HAROS was not found. Please install it first.'
+    exit 1
+fi
+
+# get the directory this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/../projects
+
+haros full -n -p fictibot.yaml
---
+++ scripts/hpl.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# check if haros is installed
+if python -c 'import pkgutil; exit(not pkgutil.find_loader("haros"))'; then
+    echo 'Found HAROS in the system.'
+else
+    echo 'HAROS was not found. Please install it first.'
+    exit 1
+fi
+
+PLUGIN="haros_plugin_pbt_gen"
+
+# check if the plugin is installed
+if python -c 'import pkgutil; exit(not pkgutil.find_loader("'$PLUGIN'"))'; then
+    echo 'Found '$PLUGIN' in the system.'
+else
+    echo $PLUGIN' was not found. Please install it first.'
+    exit 1
+fi
+
+# get the directory this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/../projects
+
+haros full -n -w $PLUGIN -p hpl.yaml
