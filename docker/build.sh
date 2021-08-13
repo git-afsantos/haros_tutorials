047d4adb5883c72e7f132cfae406cb2ddcb36c37
André Santos <contact.andre.santos@gmail.com>
2021-08-13 15:18:33 +0100

    added Docker files

---
+++ docker/build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+docker build -t fictibot $SCRIPT_DIR
+
---
+++ docker/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker run -it --tmpfs /tmp -p 8080:8080 fictibot
+
