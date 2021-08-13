c1ce012c3a44cd1710455bb151e1d7f05cfeee6a
André Santos <contact.andre.santos@gmail.com>
2021-08-13 16:50:02 +0100

    added standalone and bind versions of docker

---
+++ docker/catkin.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+
--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-docker run -it --tmpfs /tmp -p 8080:8080 fictibot
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+docker run -it \
+    --tmpfs /tmp \
+    -p 8080:8080 \
+    --mount type=bind,source="$SCRIPT_DIR"/..,target=/root/ws/src/haros_tutorials,readonly \
+    fictibot
 
