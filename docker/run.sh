4c5996d9b38431176b4ddf8839f2d6b44cafef58
André Santos <contact.andre.santos@gmail.com>
2021-08-16 14:55:44 +0100

    fixed script

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 docker run -it \
     --tmpfs /tmp \
     -p 8080:8080 \
-    --mount type=bind,source="$SCRIPT_DIR"/../src,target=/root/ws/src/haros_tutorials,readonly \
+    --mount type=bind,source="$SCRIPT_DIR"/..,target=/root/ws/src/haros_tutorials,readonly \
     fictibot
 
