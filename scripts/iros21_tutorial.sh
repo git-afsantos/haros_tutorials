f6684ae00c1476910b9c94e652089a57b41a4541
André Santos <contact.andre.santos@gmail.com>
2021-08-16 14:17:00 +0100

    added convenience script

---
+++ scripts/iros21_tutorial.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# check if haros is installed
+if python -c 'import pkgutil; exit(not pkgutil.find_loader("haros"))'; then
+    echo 'Found HAROS in the system.'
+else
+    echo 'HAROS was not found. Please install it first.'
+    exit 1
+fi
+
+# get the directory this script is in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR/../projects
+
+haros full --server-host 0.0.0.0:8080 -n -w haros_plugin_cppcheck -w haros_plugin_lizard -w haros_plugin_pyflwor -w haros_plugin_pbt_gen -p fictibot.yaml
